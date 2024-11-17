552b3beb6ee50f53446e283ac6ab45826df649db
Sean K <1682772+sk92129@users.noreply.github.com>
2024-11-16 18:02:24 -0800

    after camerax depen add

---
+++ app/src/main/java/com/kangengineering/jetpackcamera/CameraPermission.kt
@@ -0,0 +1,51 @@
+package com.kangengineering.jetpackcamera
+
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.accompanist.permissions.MultiplePermissionsState
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun Permission(
+    permission: String = android.Manifest.permission.CAMERA,
+    rationale: String = "This permission is important for this app. Please grant the permission.",
+    permissionNotAvailableContent: @Composable () -> Unit = { },
+    content: @Composable () -> Unit = { }
+) {
+    val permissionsState = rememberMultiplePermissionsState(permissions = listOf(permission))
+    if (permissionsState.allPermissionsGranted) {
+        content()
+    } else {
+        Rationale(
+            text = rationale,
+            onRequestPermission = { permissionsState.launchMultiplePermissionRequest() }
+        )
+    }
+
+}
+
+@Composable
+private fun Rationale(
+    text: String,
+    onRequestPermission: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = { /* Don't */ },
+        title = {
+            Text(text = "Permission request")
+        },
+        text = {
+            Text(text)
+        },
+        confirmButton = {
+            Button(onClick = onRequestPermission) {
+                Text("Ok")
+            }
+        }
+    )
+}
---
+++ app/src/main/java/com/kangengineering/jetpackcamera/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.kangengineering.jetpackcamera
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.kangengineering.jetpackcamera.ui.theme.JetpackCameraTheme
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+
+@ExperimentalCoilApi
+@ExperimentalCoroutinesApi
+@ExperimentalPermissionsApi
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackCameraTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    MainContent(Modifier.fillMaxSize())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackCameraTheme {
+        Greeting("Android")
+    }
+}
+
+
+
+@ExperimentalCoilApi
+@ExperimentalCoroutinesApi
+@ExperimentalPermissionsApi
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    var imageUri by remember { mutableStateOf(EMPTY_IMAGE_URI) }
+    if (imageUri != EMPTY_IMAGE_URI) {
+        Box(modifier = modifier) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = rememberImagePainter(imageUri),
+                contentDescription = "Captured image"
+            )
+            Button(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                onClick = {
+                    imageUri = EMPTY_IMAGE_URI
+                }
+            ) {
+                Text("Remove image")
+            }
+        }
+    } else {
+        var showGallerySelect by remember { mutableStateOf(false) }
+        if (showGallerySelect) {
+//            GallerySelect(
+//                modifier = modifier,
+//                onImageUri = { uri ->
+//                    showGallerySelect = false
+//                    imageUri = uri
+//                }
+//            )
+        } else {
+            Box(modifier = modifier) {
+
+//                CameraCapture(
+//                    modifier = modifier,
+//                    onImageFile = { file ->
+//                        imageUri = file.toUri()
+//                    }
+//                )
+                Button(
+                    modifier = Modifier
+                        .align(Alignment.TopCenter)
+                        .padding(4.dp),
+                    onClick = {
+                        showGallerySelect = true
+                    }
+                ) {
+                    Text("Select from Gallery")
+                }
+            }
+        }
+    }
+}
+
+val EMPTY_IMAGE_URI: Uri = Uri.parse("file://dev/null")
